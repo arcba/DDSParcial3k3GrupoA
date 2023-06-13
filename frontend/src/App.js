9b5fbd54ff57040638c8d436092f38f808cbebc4
Alejandro Rey <alejandroreyc@gmail.com>
2023-06-13 11:59:20 -0300

    parcial

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {Menu} from './components/Menu';
+import { Inicio } from './components/Inicio';
+import Articulos from './components/Articulos';
+
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+
+      <BrowserRouter>
+          <Menu />
+          <div className="divBody">
+            <Routes>
+              <Route path="/inicio" element={<Inicio />} />
+              <Route path="/articulos" element={<Articulos />} />
+
+              <Route path="*" element={<Navigate to="/inicio" replace />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+
+    
+    </div>
+  );
+}
+
+export default App;
